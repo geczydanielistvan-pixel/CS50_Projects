3b71e5d27a78d885fbb709b68b46916f3ba574c9
geczydanielistvan-pixel <geczydanielistvan@gmail.com>
2026-07-08 11:31:52 +0200

    CS50x C problem sets

--- C/me/scrabble.c
+++ C/me/scrabble.c
@@ -3,7 +3,7 @@
 #include<stdio.h>
 #include<string.h>
 
-int value[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+int value[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 1, 5, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 int sum_value(string word)
 {
 	int score = 0;
