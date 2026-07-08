a00a5b980610d75ce2206c7efeb05800446b6ce0
geczydanielistvan-pixel <geczydanielistvan@gmail.com>
2026-07-08 09:16:28 +0200

    scrabble beta 0.2

---
+++ C/me/scrabble0.2.c
@@ -0,0 +1,44 @@
+#include<cs50.h>
+#include<ctype.h>
+#include<stdio.h>
+#include<string.h>
+
+int value[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 1, 5, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+int sum_value(string word)
+{
+	int score = 0;
+	for (int i = 0, n = strlen(word); i < n; i++)
+	{ 
+		if (isupper(word[i]))
+		{
+			score += value[word[i] - 'A'];
+		}
+		else if (islower(word[i]))
+		{
+			score += value[word[i] - 'a'];
+		}
+	}
+	return score;
+}
+
+int main(void)
+{
+	string word1 = get_string("Player 1: ");
+	string word2 = get_string("Player 2: ");
+	int score1 = sum_value(word1);
+	int score2 = sum_value(word2);
+
+	if (score1 > score2)
+	{
+		printf("Player 1 wins!\n");
+	}
+	else if (score1 < score2)
+	{
+		printf("Player 2 wins!\n");
+	}
+	else
+	{
+		printf("Tie!\n");
+	}
+	return 0;
+}
